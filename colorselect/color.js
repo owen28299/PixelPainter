ddf1474d82b964a64638cca1bfcaa6deb687eb95
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-03-22 11:16:05 -1000

    more changes

---
+++ colorselect/color.js
@@ -0,0 +1,13 @@
+//adding color picker
+var pickColor;
+
+function natsColor(){
+  pickColor = document.getElementById('cp').value;
+  console.log(pickColor);
+}
+natsColor();
+
+document.getElementById('cp').addEventListener('click', function() {
+  natsColor();
+});
+
