cb0909e777c85cc37540cc126f7efe5aa6566038
Owen <owenyangg@gmail.com>
2016-05-02 19:50:01 -1000

    basic test mongoose template set up

--- server.js
+++ server.js
@@ -2,13 +2,52 @@
 
 const express = require('express'),
       app     = express(),
-      bodyParser = require('body-parser')
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose')
       ;
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.once('open', function() {
+  console.log("database opened");
+
+  var imageSchema = mongoose.Schema({
+    name: String
+  });
+
+  var Image = mongoose.model('Image', imageSchema);
+
+  app.get("/testimage", function(req,res){
+   var image2 = new Image({name: "Circle"});
+   image2.save(function(err, data){
+    if(err) {
+      res.send(err);
+    }
+    else {
+      res.send(data);
+    }
+   });
+  });
+
+  app.get("/gettestimage", function(req,res){
+    Image.find( {name: "Circle"}, function(err,images){
+      if(err) {
+        res.send(err);
+      }
+      else {
+        res.send(images);
+      }
+    });
+  });
+
+});
+
 
 var image = {};
 
